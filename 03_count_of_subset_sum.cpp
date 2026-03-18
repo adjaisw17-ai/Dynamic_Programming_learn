224c5222df0c09e7c007be75d915a7f69bd51ab2
adjaisw17-ai <adjaisw17@gmail.com>
2026-03-18 23:21:49 +0530

    Add files via upload

---
+++ 03_count_of_subset_sum.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(){
+    int arr[]={2,3,5,8,10};
+    int n=5;
+    int sum=10;
+    bool t[n+1][sum+1];
+    for(int i=0;i<n+1;i++){
+        for(int j=0;j<sum+1;j++){
+            if(i==0) t[i][j]=false;
+            if(j==0) t[i][j]=true;
+        }
+    }
+    for(int i=1;i<n+1;i++){
+        for(int j=1;j<sum+1;j++){
+            if(arr[i-1]<=j){
+                t[i][j]=t[i-1][j-arr[i-1]] || t[i-1][j];
+            }
+            else{
+                t[i][j]=t[i-1][j];
+            }
+        }
+    }
+    
+    for(int i=0;i<n+1;i++){
+        for(int j=0;j<sum+1;j++){
+            cout<<"("<<i<<","<<j<<")="<<t[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    int count=0;
+    for(int i=1;i<n+1;i++){
+        if(t[i][10]==true) count++;
+    }
+    return count;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    {
+       cout<<solve()<<endl;
+    }
+    return 0;
+}
