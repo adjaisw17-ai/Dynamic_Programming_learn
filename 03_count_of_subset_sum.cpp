dd9246a57c29a3eff8031aad0bd101f97d3b8314
adjaisw17-ai <adjaisw17@gmail.com>
2026-03-19 19:55:33 +0530

    Update 03_count_of_subset_sum.cpp

--- 03_count_of_subset_sum.cpp
+++ 03_count_of_subset_sum.cpp
@@ -5,17 +5,17 @@ int solve(){
     int arr[]={2,3,5,8,10};
     int n=5;
     int sum=10;
-    bool t[n+1][sum+1];
+    int t[n+1][sum+1];
     for(int i=0;i<n+1;i++){
         for(int j=0;j<sum+1;j++){
-            if(i==0) t[i][j]=false;
-            if(j==0) t[i][j]=true;
+            if(i==0) t[i][j]=0;
+            if(j==0) t[i][j]=1;
         }
     }
     for(int i=1;i<n+1;i++){
         for(int j=1;j<sum+1;j++){
             if(arr[i-1]<=j){
-                t[i][j]=t[i-1][j-arr[i-1]] || t[i-1][j];
+                t[i][j]=t[i-1][j-arr[i-1]] + t[i-1][j];
             }
             else{
                 t[i][j]=t[i-1][j];
@@ -29,11 +29,7 @@ int solve(){
         }
         cout<<endl;
     }
-    int count=0;
-    for(int i=1;i<n+1;i++){
-        if(t[i][10]==true) count++;
-    }
-    return count;
+    return t[n][10];
 }
 
 int main() {
